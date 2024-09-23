2bdbea1c1db417ec9bf6f14e7ea3f1c05254a2b6
vikas-11-dixena <vikasdixena79@gmail.com>
2024-09-23 12:28:58 +0530

    component is completed (sidebar also created)

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -21,7 +21,7 @@ function Gallery() {
     <GalleryStyled>
         <div className='back'>
           <Link to="/">
-            <i class="fa-solid fa-arrow-left"></i>
+            <i className="fa-solid fa-arrow-left"></i>
               Back to home
           </Link>
         </div>
